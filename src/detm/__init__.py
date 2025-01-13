cdb730235a61c6e79123eb88b3248e57477d405a
efi-github <elisabeth.fittschen@gmail.com>
2025-01-13 06:51:11 -0500

    -

--- src/detm/__init__.py
+++ src/detm/__init__.py
@@ -1,4 +1,6 @@
 from .original import DETM
+from .CETM_MLP import CETM_MLP
+from .DBETM import DBETM
 from .corpus import Corpus
 from .embeddings import train_embeddings, load_embeddings, save_embeddings, filter_embeddings
 from .utils import train_model, perplexity_on_corpus
