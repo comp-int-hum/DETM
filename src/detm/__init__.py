b91ec9d42e894de0475c07ff64cddad1b8349557
efi-github <elisabeth.fittschen@gmail.com>
2025-02-17 06:54:19 -0500

    adding dummy class for reweight_losses=True

--- src/detm/__init__.py
+++ src/detm/__init__.py
@@ -10,3 +10,6 @@ from .corpus import Corpus
 from .abstract_detm import AbstractDETM
 from .embeddings import train_embeddings, load_embeddings, save_embeddings
 from .utils import train_model, apply_model
+from functools import partial
+
+xDETMcoeff = partial(xDETM, reweight_losses=True)
