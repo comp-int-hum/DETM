792bcfcb2f32c3be8823132e623ea888d93575f5
efi-github <elisabeth.fittschen@gmail.com>
2025-02-19 12:48:19 -0500

    adding evaluation functions to init file

--- src/detm/__init__.py
+++ src/detm/__init__.py
@@ -11,5 +11,6 @@ from .abstract_detm import AbstractDETM
 from .embeddings import train_embeddings, load_embeddings, save_embeddings
 from .utils import train_model, apply_model
 from functools import partial
+from .evaluations import evaluate_coherence, evaluate_topic_diversity
 
 xDETMcoeff = partial(xDETM, reweight_losses=True)
